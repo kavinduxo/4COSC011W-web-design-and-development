551611f6c39e67379449594001f0607c3bbba2c6
SadeshS <sadesh.2019815@iit.ac.lk>
2021-03-20 21:57:42 +0530

    Issue #9 thumbnail images

---
+++ js/where-to-go.js
@@ -0,0 +1,71 @@
+document.getElementById('figureCP').addEventListener('mouseover', function(){
+    document.getElementById('CP').style.display = "block";
+})
+
+document.getElementById('figureCP').addEventListener('mouseleave', function(){
+    document.getElementById('CP').style.display = "none";
+})
+
+document.getElementById('figureEP').addEventListener('mouseover', function(){
+    document.getElementById('EP').style.display = "block";
+})
+
+document.getElementById('figureEP').addEventListener('mouseleave', function(){
+    document.getElementById('EP').style.display = "none";
+})
+
+document.getElementById('figureNCP').addEventListener('mouseover', function(){
+    document.getElementById('NCP').style.display = "block";
+})
+
+document.getElementById('figureNCP').addEventListener('mouseleave', function(){
+    document.getElementById('NCP').style.display = "none";
+})
+
+document.getElementById('figureNWP').addEventListener('mouseover', function(){
+    document.getElementById('NWP').style.display = "block";
+})
+
+document.getElementById('figureNWP').addEventListener('mouseleave', function(){
+    document.getElementById('NWP').style.display = "none";
+})
+
+document.getElementById('figureNP').addEventListener('mouseover', function(){
+    document.getElementById('NP').style.display = "block";
+})
+
+document.getElementById('figureNP').addEventListener('mouseleave', function(){
+    document.getElementById('NP').style.display = "none";
+})
+
+document.getElementById('figureSaP').addEventListener('mouseover', function(){
+    document.getElementById('SaP').style.display = "block";
+})
+
+document.getElementById('figureSaP').addEventListener('mouseleave', function(){
+    document.getElementById('SaP').style.display = "none";
+})
+
+document.getElementById('figureSP').addEventListener('mouseover', function(){
+    document.getElementById('SP').style.display = "block";
+})
+
+document.getElementById('figureSP').addEventListener('mouseleave', function(){
+    document.getElementById('SP').style.display = "none";
+})
+
+document.getElementById('figureUP').addEventListener('mouseover', function(){
+    document.getElementById('UP').style.display = "block";
+})
+
+document.getElementById('figureUP').addEventListener('mouseleave', function(){
+    document.getElementById('UP').style.display = "none";
+})
+
+document.getElementById('figureWP').addEventListener('mouseover', function(){
+    document.getElementById('WP').style.display = "block";
+})
+
+document.getElementById('figureWP').addEventListener('mouseleave', function(){
+    document.getElementById('WP').style.display = "none";
+})
