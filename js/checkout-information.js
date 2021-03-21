7d5216730a8effdc10aff580c093dc43c6314c41
infazrumy <rumy.2019818@iit.ac.lk>
2021-03-21 21:37:16 +0530

    buy product completed

--- js/checkout-information.js
+++ js/checkout-information.js
@@ -1,6 +1,6 @@
-let modal = document.getElementById("myModal");
+let modal = document.getElementById("Modal");
 
-let btn = document.getElementById("myBtn");
+let btn = document.getElementById("Btn");
 
 let span = document.getElementsByClassName("close")[0];
 
@@ -19,6 +19,14 @@ window.onclick = function(event) {
 }
 
 
+let confirmOrder = document.getElementById('confirm-order');
+
+let isNullFieldsAvailable, isContactNumberValid, isEmailValid;
+
+confirmOrder.addEventListener('click', () => {
+    ValidateCustomerInformation();
+})
+
 function ValidateCustomerInformation() {
 
     let city = document.querySelector('.city').value;
@@ -28,29 +36,87 @@ function ValidateCustomerInformation() {
     let address = document.querySelector('.address').value;
     let contactNumber = document.querySelector('.telephone').value;
 
-    if(firstName === "" || lastName === "" || email=== "" || city === "" || address === "" || contactNumber === "") {
-        alert('All Fields should be filled');
-    }else  {
-//        displayInvoice(firstName, lastName, email, contactNumber);
+    isNullFieldsAvailable = checkForNullValues(firstName, lastName, email, contactNumber, city, address);
 
+    if(isNullFieldsAvailable){
+        isContactNumberValid = validateContactNumber(contactNumber);
+        if(!isContactNumberValid) {
+            alert('Invalid Contact Number');
+        }
+        isEmailValid = validateEmail(email);
+        if(!isEmailValid) {
+            alert('Invalid Email');
+        }
     }
 
+        if(isNullFieldsAvailable && isContactNumberValid && isEmailValid) {
+            displayInvoice(firstName, lastName, email, contactNumber);
+        }
 }
 
-function ClearCart(){
-    sessionStorage.clear();
+function checkForNullValues(firstName, lastName, email, contactNumber, city, address) {
+
+    if(firstName === "" || lastName === "" || email=== "" || city === "" || address === "" || contactNumber === "") {
+        alert('All Fields should be filled');
+        return false;
+    }else {
+        return true;
+    }
 }
+
+function validateEmail(email) {
+
+    let atSymbol = email.indexOf("@");
+    if(atSymbol < 1)  return false;
+
+    let dot = email.indexOf(".");
+    if(dot <= atSymbol + 2) return false;
+
+    if (dot === email.length - 1) return false;
+
+    return true;
+}
+
+function validateContactNumber(contactNumber) {
+    if(contactNumber.length !== 10 || isNaN(contactNumber)){
+        return false;
+    }else {
+        return true;
+    }
+}
+
 
 function displayInvoice(firstName, lastName, email, contactNumber) {
-    let productInvoice = document.querySelector('modal-content')
+    let productInvoice = document.querySelector('.modal-content')
+
     productInvoice.innerHTML = `
-            <h3>Invoice</h3>
+            <button id="Btn" onClick="ClearEnv()">Thanks for Shopping</button>
+            <br style="margin-top: 25px">
+            <h1>Invoice</h1>
+            <hr>
             <h5>Customer Name: ${firstName +' '+ lastName}</h5>
             <h5>Customer Mail: ${email}</h5>
             <h5>Customer Contact Number: ${contactNumber}</h5>
-            <h5>Total Bill: $${sessionStorage.getItem('totalCartCost')}</h5> 
+            <h5>Total Bill: $${sessionStorage.getItem('totalCartCost')}</h5>
+            <hr>
+            <h3>Break Down</h3>    
             <br>
-            <button>Thanks for Shopping</button>
-`
+             
+`;
+    Object.values(JSON.parse(sessionStorage.getItem('productsInCart'))).map(item => {
+            productInvoice.innerHTML += `
+            <h5>Product Name:  ${item.productName}</h5>
+            <h5>Product Price: ${item.productPrice}</h5>
+            <h5>Quantity: ${item.productsInCart}</h5>
+            <hr>    
+`;
 
+
+    });
+}
+
+
+function ClearEnv() {
+    sessionStorage.clear();
+    location.reload();
 }
